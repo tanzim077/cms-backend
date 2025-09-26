df7e99c9c1c909a0a7bc889db0601a64c14a3321
tanzim077 <tanzimahmed1994@gmail.com>
2025-09-26 12:14:45 +0600

    feat: add Swagger examples for user, role, and permission creation requests and responses

--- apps/api/src/examples/swagger-examples.ts
+++ apps/api/src/examples/swagger-examples.ts
@@ -60,3 +60,393 @@ export const registrationResponseExample = {
   createdAt: '2025-09-24T10:45:32.730Z',
   updatedAt: '2025-09-24T10:45:32.730Z',
 };
+
+export const permissionCreateRequestExample = {
+  example1: {
+    summary: 'Login Example',
+    value: {
+      email: 'admin@gmail.com',
+      password: '12345',
+    },
+  },
+};
+export const permissionCreateResponseExample = {
+  access_token: '',
+  refresh_token: '',
+  user: {
+    id: 1,
+    name: 'Tanzim Ahmed',
+    email: 'admin@gmail.com',
+    image: null,
+    address: null,
+    phone: null,
+    is_verified: false,
+    is_blocked: false,
+    nid_no: null,
+    verication_document: null,
+    is_deleted: false,
+    is_active: true,
+    createdAt: '2025-09-21T07:09:51.533Z',
+    updatedAt: '2025-09-24T08:32:59.956Z',
+  },
+};
+
+export const roleCreateRequestExample = {
+  example1: {
+    summary: 'Login Example',
+    value: {
+      email: 'admin@gmail.com',
+      password: '12345',
+    },
+  },
+};
+export const roleCreateResponseExample = {
+  access_token: '',
+  refresh_token: '',
+  user: {
+    id: 1,
+    name: 'Tanzim Ahmed',
+    email: 'admin@gmail.com',
+    image: null,
+    address: null,
+    phone: null,
+    is_verified: false,
+    is_blocked: false,
+    nid_no: null,
+    verication_document: null,
+    is_deleted: false,
+    is_active: true,
+    createdAt: '2025-09-21T07:09:51.533Z',
+    updatedAt: '2025-09-24T08:32:59.956Z',
+  },
+};
+
+export const classCreateRequestExample = {
+  example1: {
+    summary: 'Login Example',
+    value: {
+      email: 'admin@gmail.com',
+      password: '12345',
+    },
+  },
+};
+export const classCreateResponseExample = {
+  access_token: '',
+  refresh_token: '',
+  user: {
+    id: 1,
+    name: 'Tanzim Ahmed',
+    email: 'admin@gmail.com',
+    image: null,
+    address: null,
+    phone: null,
+    is_verified: false,
+    is_blocked: false,
+    nid_no: null,
+    verication_document: null,
+    is_deleted: false,
+    is_active: true,
+    createdAt: '2025-09-21T07:09:51.533Z',
+    updatedAt: '2025-09-24T08:32:59.956Z',
+  },
+};
+
+export const moduleCreateRequestExample = {
+  example1: {
+    summary: 'Login Example',
+    value: {
+      email: 'admin@gmail.com',
+      password: '12345',
+    },
+  },
+};
+export const moduleCreateResponseExample = {
+  access_token: '',
+  refresh_token: '',
+  user: {
+    id: 1,
+    name: 'Tanzim Ahmed',
+    email: 'admin@gmail.com',
+    image: null,
+    address: null,
+    phone: null,
+    is_verified: false,
+    is_blocked: false,
+    nid_no: null,
+    verication_document: null,
+    is_deleted: false,
+    is_active: true,
+    createdAt: '2025-09-21T07:09:51.533Z',
+    updatedAt: '2025-09-24T08:32:59.956Z',
+  },
+};
+
+export const quizCreateRequestExample = {
+  example1: {
+    summary: 'Login Example',
+    value: {
+      email: 'admin@gmail.com',
+      password: '12345',
+    },
+  },
+};
+export const quizCreateResponseExample = {
+  access_token: '',
+  refresh_token: '',
+  user: {
+    id: 1,
+    name: 'Tanzim Ahmed',
+    email: 'admin@gmail.com',
+    image: null,
+    address: null,
+    phone: null,
+    is_verified: false,
+    is_blocked: false,
+    nid_no: null,
+    verication_document: null,
+    is_deleted: false,
+    is_active: true,
+    createdAt: '2025-09-21T07:09:51.533Z',
+    updatedAt: '2025-09-24T08:32:59.956Z',
+  },
+};
+
+export const questionBankCreateRequestExample = {
+  example1: {
+    summary: 'Login Example',
+    value: {
+      email: 'admin@gmail.com',
+      password: '12345',
+    },
+  },
+};
+export const questionBankResponseExample = {
+  access_token: '',
+  refresh_token: '',
+  user: {
+    id: 1,
+    name: 'Tanzim Ahmed',
+    email: 'admin@gmail.com',
+    image: null,
+    address: null,
+    phone: null,
+    is_verified: false,
+    is_blocked: false,
+    nid_no: null,
+    verication_document: null,
+    is_deleted: false,
+    is_active: true,
+    createdAt: '2025-09-21T07:09:51.533Z',
+    updatedAt: '2025-09-24T08:32:59.956Z',
+  },
+};
+
+export const userCreateRequestExample = {
+  example1: {
+    summary: 'Login Example',
+    value: {
+      email: 'admin@gmail.com',
+      password: '12345',
+    },
+  },
+};
+export const userCreateResponseExample = {
+  access_token: '',
+  refresh_token: '',
+  user: {
+    id: 1,
+    name: 'Tanzim Ahmed',
+    email: 'admin@gmail.com',
+    image: null,
+    address: null,
+    phone: null,
+    is_verified: false,
+    is_blocked: false,
+    nid_no: null,
+    verication_document: null,
+    is_deleted: false,
+    is_active: true,
+    createdAt: '2025-09-21T07:09:51.533Z',
+    updatedAt: '2025-09-24T08:32:59.956Z',
+  },
+};
+
+export const bulkUsersCreateRequestExample = {
+  example1: {
+    summary: 'Login Example',
+    value: {
+      email: 'admin@gmail.com',
+      password: '12345',
+    },
+  },
+};
+export const bulkUsersResponseExample = {
+  access_token: '',
+  refresh_token: '',
+  user: {
+    id: 1,
+    name: 'Tanzim Ahmed',
+    email: 'admin@gmail.com',
+    image: null,
+    address: null,
+    phone: null,
+    is_verified: false,
+    is_blocked: false,
+    nid_no: null,
+    verication_document: null,
+    is_deleted: false,
+    is_active: true,
+    createdAt: '2025-09-21T07:09:51.533Z',
+    updatedAt: '2025-09-24T08:32:59.956Z',
+  },
+};
+
+export const reviewCreateRequestExample = {
+  example1: {
+    summary: 'Login Example',
+    value: {
+      email: 'admin@gmail.com',
+      password: '12345',
+    },
+  },
+};
+export const reviewCreateResponseExample = {
+  access_token: '',
+  refresh_token: '',
+  user: {
+    id: 1,
+    name: 'Tanzim Ahmed',
+    email: 'admin@gmail.com',
+    image: null,
+    address: null,
+    phone: null,
+    is_verified: false,
+    is_blocked: false,
+    nid_no: null,
+    verication_document: null,
+    is_deleted: false,
+    is_active: true,
+    createdAt: '2025-09-21T07:09:51.533Z',
+    updatedAt: '2025-09-24T08:32:59.956Z',
+  },
+};
+
+export const blogCreateRequestExample = {
+  example1: {
+    summary: 'Login Example',
+    value: {
+      email: 'admin@gmail.com',
+      password: '12345',
+    },
+  },
+};
+export const blogCreateResponseExample = {
+  access_token: '',
+  refresh_token: '',
+  user: {
+    id: 1,
+    name: 'Tanzim Ahmed',
+    email: 'admin@gmail.com',
+    image: null,
+    address: null,
+    phone: null,
+    is_verified: false,
+    is_blocked: false,
+    nid_no: null,
+    verication_document: null,
+    is_deleted: false,
+    is_active: true,
+    createdAt: '2025-09-21T07:09:51.533Z',
+    updatedAt: '2025-09-24T08:32:59.956Z',
+  },
+};
+
+export const paymentCreateRequestExample = {
+  example1: {
+    summary: 'Login Example',
+    value: {
+      email: 'admin@gmail.com',
+      password: '12345',
+    },
+  },
+};
+export const paymentCreateResponseExample = {
+  access_token: '',
+  refresh_token: '',
+  user: {
+    id: 1,
+    name: 'Tanzim Ahmed',
+    email: 'admin@gmail.com',
+    image: null,
+    address: null,
+    phone: null,
+    is_verified: false,
+    is_blocked: false,
+    nid_no: null,
+    verication_document: null,
+    is_deleted: false,
+    is_active: true,
+    createdAt: '2025-09-21T07:09:51.533Z',
+    updatedAt: '2025-09-24T08:32:59.956Z',
+  },
+};
+
+export const discussionCreateRequestExample = {
+  example1: {
+    summary: 'Login Example',
+    value: {
+      email: 'admin@gmail.com',
+      password: '12345',
+    },
+  },
+};
+export const discussionCreateResponseExample = {
+  access_token: '',
+  refresh_token: '',
+  user: {
+    id: 1,
+    name: 'Tanzim Ahmed',
+    email: 'admin@gmail.com',
+    image: null,
+    address: null,
+    phone: null,
+    is_verified: false,
+    is_blocked: false,
+    nid_no: null,
+    verication_document: null,
+    is_deleted: false,
+    is_active: true,
+    createdAt: '2025-09-21T07:09:51.533Z',
+    updatedAt: '2025-09-24T08:32:59.956Z',
+  },
+};
+
+export const configurationCreateRequestExample = {
+  example1: {
+    summary: 'Login Example',
+    value: {
+      email: 'admin@gmail.com',
+      password: '12345',
+    },
+  },
+};
+export const configurationCreateResponseExample = {
+  access_token: '',
+  refresh_token: '',
+  user: {
+    id: 1,
+    name: 'Tanzim Ahmed',
+    email: 'admin@gmail.com',
+    image: null,
+    address: null,
+    phone: null,
+    is_verified: false,
+    is_blocked: false,
+    nid_no: null,
+    verication_document: null,
+    is_deleted: false,
+    is_active: true,
+    createdAt: '2025-09-21T07:09:51.533Z',
+    updatedAt: '2025-09-24T08:32:59.956Z',
+  },
+};
